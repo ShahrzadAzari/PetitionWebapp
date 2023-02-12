8a778566aaa3410b072ce0b2c74c8326a9b02cad
Shahrzad <shahrzad123azari@gmail.com>
2023-02-12 11:37:55 +0330

    change urls

--- PetitionWebapp/user_app/urls.py
+++ PetitionWebapp/user_app/urls.py
@@ -5,10 +5,10 @@ app_name='user_app'
 
 urlpatterns = [
     path('signup/', UserSignUpView.as_view(), name='user-signup'),
-    path('<int:pk>/profile/update/', UserUpdateView.as_view(), name='profile-update'),
+    path('<int:pk>/profile-update/', UserUpdateView.as_view(), name='profile-update'),
     path('<int:pk>/detail/', UserDetailView.as_view(), name='user-detail'),
     path('list/', UserListView.as_view(), name='user-list'),
     path('login/', LoginView.as_view(), name='user-login'),
     path('logout/', LogoutView.as_view(), name='user-logout'),
-    path('<int:pk>/profile/update/password/', ChangePasswordView.as_view(), name='profile-password'),
+    path('<int:pk>/update-password/', ChangePasswordView.as_view(), name='profile-password'),
 ]
